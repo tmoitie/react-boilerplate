8237ea2c6d4bab2968f8f8f1d66abe1721e49095
Tom Moitié <tomm@magmadigital.co.uk>
2016-12-14 17:52:57 +0000

    Reset App comment

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import './styles.scss';
 
 /**
- * Sets up the main page view and stores the state of filtering/ordering
+ * Sets up the main page view
  */
 export default class App extends Component {
   render() {
