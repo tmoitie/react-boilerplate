c19cd97f6a86ce80f4dbe9e275a26827d7d1c7b6
Tom Moitié <tomm@magmadigital.co.uk>
2016-12-14 17:46:59 +0000

    Update react-hot-loader

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
+/* eslint "global-require": 0 */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 
 import App from './containers/App';
 
 if (process.env.NODE_ENV === 'development') {
-  /* eslint "global-require": 0 */
   require('./devTools');
 }
 
 ReactDOM.render(
-  <App />,
+  <AppContainer><App /></AppContainer>,
   document.getElementById('root')
 );
+
+if (module.hot) {
+  module.hot.accept('./containers/App', () => {
+    const NextApp = require('./containers/App').default;
+
+    ReactDOM.render(
+      <AppContainer>
+        <NextApp />
+      </AppContainer>,
+      document.getElementById('root')
+    );
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
   devtool: env === 'production' ? null : 'eval-source-maps',
   entry: {
     main: env === 'production' ? ['./src/index'] : [
+      'react-hot-loader/patch',
       `webpack-dev-server/client?http://localhost:${port}`,
       'webpack/hot/only-dev-server',
       './src/index'
@@ -26,7 +27,7 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.js$/,
-      loaders: env === 'production' ? ['babel'] : ['react-hot', 'babel'],
+      loaders: ['babel'],
       include: [path.join(__dirname, 'src')]
     }, {
       test: /\.scss$/,
